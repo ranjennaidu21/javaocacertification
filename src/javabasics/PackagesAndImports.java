703f09889caf1745f25e1eb7513e5e5bf3a827f8
ranjen <ranjen@myboost.com.my>
2019-09-27 18:03:35 +0800

    Imports Naming Conflict and Code Formatting

--- src/javabasics/PackagesAndImports.java
+++ src/javabasics/PackagesAndImports.java
@@ -1,6 +1,12 @@
 package javabasics;
 //the random class need to be imported
+
+import java.util.Date;
 import java.util.Random;
+
+//for the date if both using same class name better to ignore wildcard and use fullName as below,
+//or will conflict
+//import java.sql.*;
 
 public class PackagesAndImports {
 
@@ -8,6 +14,12 @@ public class PackagesAndImports {
         //for System dont have to import as it is in java.lang class
         Random random = new Random();
         System.out.println(random.nextInt(5));
+
+        Date date;
+        java.sql.Date date2;
     }
+
+    //shortcut ctrl+alt+l to reformat code
+    //shortcut ctrl+alt+o to organize and clear unused import
 }
 
